4ceee58fa7016be47765120421d52760fc12aa80
Andrew3369 <andrewjanosbabos@gmail.com>
2023-10-10 00:43:19 -0400

    First version of Visual++! Added in menu options at the top, along with the document name. Open, Save, Save As, Exit all work. Cut, Copy, Paste work as well.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Visual__
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private bool isSaved = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            TextBox.Text =
+                "Welcome to Visual++ !\n" +
+                "The new general text editor and IDE!\n" +
+                "Everything you need to get started is above!";
+            DocumentName.Text = "Untitled";
+        }
+
+        private void OnClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void IsSaved(object sender, TextChangedEventArgs e)
+        {
+            isSaved = false;
+            if (DocumentName.Text == "Untitled")
+            {
+                DocumentName.Text = "Untitled*";
+            }
+            else
+            {
+                DocumentName.Text += "*";
+            }
+        }
+    }
+}
